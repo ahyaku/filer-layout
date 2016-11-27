e2fc06e91322e5d1e4b8b1a0d3e1eeb325ae5e21
ahyaku <ahyahyaku@gmail.com>
2016-11-28 02:05:42 +0900

    Add display area for each item's ext, size, date and time with adding ellipsis feature to name.

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -32,20 +32,110 @@ const ItemList = () => {
   const len = 100;
   let items = new Array(len);
   for(let i=0; i<len; i++){
-    items[i] = 'hoge';
+    items[i] = 'Since the dawn of history';
   }
+
+  //return (
+  //  <div style={style}>
+  //    {items.map((e, i) => {
+  //      return (
+  //        <div key={i}>
+  //          {e}
+  //        </div>
+  //      );
+  //    })}
+  //  </div>
+  //);
+
+  const style_each_item = {
+    display: 'flex',
+    flex: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  };
+
+  const style_name = {
+    marginRight: 'auto',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflowX: 'hidden'
+  };
+
+  const style_info = {
+    display: 'flex',
+    flex: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  };
+
+  //return (
+  //  <div style={style}>
+  //    {items.map((e, i) => {
+  //      return (
+  //        <ItemView key={i}
+  //                  name={e}
+  //                  />
+  //      );
+  //    })}
+  //  </div>
+  //);
 
   return (
     <div style={style}>
       {items.map((e, i) => {
         return (
-          <div key={i}>
-            {e}
+          <div key={i} >
+            <div style={style_each_item}>
+              <div style={style_name}>
+                {e}
+              </div>
+              <div style={style_info}>
+                <div>
+                  .ext
+                </div>
+                <div>
+                  size
+                </div>
+                <div>
+                  date
+                </div>
+                <div>
+                  time
+                </div>
+              </div>
+            </div>
           </div>
         );
       })}
     </div>
   );
+}
+
+class ItemView extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    const style_each_item = {
+      display: 'flex',
+      flex: 'auto',
+      flexDirection: 'row',
+      justifyContent: 'flex-start'
+    }
+
+    return (
+      <div style={style_each_item}>
+        <div>
+          {this.props.name}
+        </div>
+        <div>
+          exe
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ItemList;
