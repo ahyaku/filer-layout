06c0385fffe90fa9d23b916038573eca0ec327d1
ahyaku <ahyahyaku@gmail.com>
2016-11-29 00:10:29 +0900

    Refoctor with sub classes.

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -3,19 +3,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-//class Body extends React.Component {
-//  constructor(props){
-//    super(props);
-//  }
-//  render(){
-//    return(
-//      <div>
-//        Body
-//      </div>
-//    );
-//  }
-//}
-
 const ItemList = () => {
   const style = {
     border: 'solid 1px #FFFFFF',
@@ -35,76 +22,16 @@ const ItemList = () => {
     items[i] = 'Since the dawn of history';
   }
 
-  //return (
-  //  <div style={style}>
-  //    {items.map((e, i) => {
-  //      return (
-  //        <div key={i}>
-  //          {e}
-  //        </div>
-  //      );
-  //    })}
-  //  </div>
-  //);
-
-  const style_each_item = {
-    display: 'flex',
-    flex: 'auto',
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
-  };
-
-  const style_name = {
-    marginRight: 'auto',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    overflowX: 'hidden'
-  };
-
-  const style_info = {
-    display: 'flex',
-    flex: 'auto',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  };
-
-  //return (
-  //  <div style={style}>
-  //    {items.map((e, i) => {
-  //      return (
-  //        <ItemView key={i}
-  //                  name={e}
-  //                  />
-  //      );
-  //    })}
-  //  </div>
-  //);
-
   return (
     <div style={style}>
       {items.map((e, i) => {
         return (
-          <div key={i} >
-            <div style={style_each_item}>
-              <div style={style_name}>
-                {e}
-              </div>
-              <div style={style_info}>
-                <div>
-                  .ext
-                </div>
-                <div>
-                  size
-                </div>
-                <div>
-                  date
-                </div>
-                <div>
-                  time
-                </div>
-              </div>
-            </div>
-          </div>
+          <ItemView key={i}
+                    name={e}
+                    ext={'.ext'}
+                    size={'size'}
+                    date={'date'}
+                    time={'time'}/>
         );
       })}
     </div>
@@ -117,22 +44,90 @@ class ItemView extends React.Component{
   }
 
   render(){
-
     const style_each_item = {
       display: 'flex',
       flex: 'auto',
       flexDirection: 'row',
       justifyContent: 'flex-start'
+    };
+
+    return (
+      <div>
+        <div style={style_each_item}>
+          <Name name={this.props.name}/>
+          <Props ext={this.props.ext}
+                 size={this.props.size}
+                 date={this.props.date}
+                 time={this.props.time}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Name extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const style = {
+      marginRight: 'auto',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflowX: 'hidden'
+    };
+
+    return (
+      <div style={style}>
+        {this.props.name}
+      </div>
+    );
+  }
+}
+
+class Props extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const style = {
+      display: 'flex',
+      flex: 'auto',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    };
+
+    const margin_left_ext = '0px';
+    const margin_left_size = '15px';
+    const margin_left_date = '5px';
+    const margin_left_time = '5px';
+
+    return (
+      <div style={style}>
+        <PropElem elem={this.props.ext} margin_left={margin_left_ext}/>
+        <PropElem elem={this.props.size} margin_left={margin_left_size}/>
+        <PropElem elem={this.props.date} margin_left={margin_left_date}/>
+        <PropElem elem={this.props.time} margin_left={margin_left_time}/>
+      </div>
+    );
+  }
+}
+
+class PropElem extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const style={
+      marginLeft: this.props.margin_left
     }
 
     return (
-      <div style={style_each_item}>
-        <div>
-          {this.props.name}
-        </div>
-        <div>
-          exe
-        </div>
+      <div style={style}>
+        {this.props.elem}
       </div>
     );
   }
